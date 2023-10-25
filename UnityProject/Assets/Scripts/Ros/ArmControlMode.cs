a084358b8cfa184c695f90f8504ff803f0690f99
kota0823mac <kota.study.teleg14@gmail.com>
2023-10-26 06:38:52 +0900

    create Arm control msg and controling scripts.

---
+++ UnityProject/Assets/Scripts/Ros/ArmControlMode.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum ArmControlMode
+{
+    Sleep=0,
+    Home=1,
+
+    TrackTarget=100,
+}
--- UnityProject/Assets/Scripts/buttonControl.cs
+++ UnityProject/Assets/Scripts/buttonControl.cs
@@ -4,6 +4,14 @@ using UnityEngine;
 
 public class buttonControl : MonoBehaviour
 {
+
+    public enum ArmControlMode
+    {
+        Sleep = 0,
+        Home = 1,
+
+        TrackTarget = 100,
+    }
     // Start is called before the first frame update
     void Start()
     {
