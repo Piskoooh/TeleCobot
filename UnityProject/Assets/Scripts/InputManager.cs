b8725d136c74d4437aa7d79ed5304fcebcc24171
kota0823mac <kota.study.teleg14@gmail.com>
2023-11-10 02:31:19 +0900

    Modify scripts which enables Joy control from unity

--- UnityProject/Assets/Scripts/InputManager.cs
+++ UnityProject/Assets/Scripts/InputManager.cs
@@ -3,10 +3,68 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
+//InputSystemに入力(Action)があった際に行う処理をまとめたスクリプト
+//有効化するActionMapの切り替えもここで制御するかも。
 public class InputManager : MonoBehaviour
 {
     [HideInInspector]
-    public bool armHomePose, armSleepPose;
+    public bool speedInc,speedDec,speedCourse,speedFine,gripperOpen, gripperClose,
+        armHomePose, armSleepPose, panTiltHome, switchMode;
+
+    [HideInInspector]
+    public float gripperPwm, waistRotate, eeZ, eeY, baseX, baseRotate, eeRoll, eePitch, pan, tilt;
+
+    public void OnGripperPwm(InputAction.CallbackContext context)
+    {
+        Debug.Log("OnGripperPwm called");
+        if (context.action.triggered)
+        {
+            gripperPwm = context.ReadValue<float>();
+        }
+        else if (context.canceled)
+        {
+            gripperPwm = 0;
+        }
+    }
+
+    public void OnGripperOpen(InputAction.CallbackContext context)
+    {
+        Debug.Log("OnGripperOpen called");
+        if (context.action.triggered)
+        {
+            gripperOpen = context.ReadValueAsButton();
+        }
+        else if (context.canceled)
+        {
+            gripperOpen = false;
+        }
+    }
+
+    public void OnGripperClose(InputAction.CallbackContext context)
+    {
+        Debug.Log("OnGripperClose called");
+        if (context.action.triggered)
+        {
+            gripperClose = context.ReadValueAsButton();
+        }
+        else if (context.canceled)
+        {
+            gripperClose = false;
+        }
+    }
+
+    public void OnWaistRotate(InputAction.CallbackContext context)
+    {
+        Debug.Log("OnWaistRotate called");
+        if (context.action.triggered)
+        {
+            waistRotate = context.ReadValue<float>();
+        }
+        else if (context.canceled)
+        {
+            waistRotate = 0;
+        }
+    }
 
     public void OnArmHomePose(InputAction.CallbackContext context)
     {
@@ -31,6 +89,123 @@ public class InputManager : MonoBehaviour
         else if (context.canceled)
         {
             armSleepPose = false;
+        }
+    }
+
+    public void OnPanTiltHome(InputAction.CallbackContext context)
+    {
+        Debug.Log("OnPanTiltHome called");
+        if (context.action.triggered)
+        {
+            panTiltHome = context.ReadValueAsButton();
+        }
+        else if (context.canceled)
+        {
+            panTiltHome = false;
+        }
+    }
+
+    public void OnSwitchMode(InputAction.CallbackContext context)
+    {
+        Debug.Log("OnSwitchMode called");
+        if (context.action.triggered)
+        {
+            switchMode = context.ReadValueAsButton();
+        }
+        else if (context.canceled)
+        {
+            switchMode = false;
+        }
+    }
+
+    public void OnEeZ(InputAction.CallbackContext context)
+    {
+        Debug.Log("OnEeZ called");
+        if (context.action.triggered)
+        {
+            eeZ = context.ReadValue<float>();
+        }
+        else if (context.canceled)
+        {
+            eeZ = 0;
+        }
+    }
+
+    public void OnEeY(InputAction.CallbackContext context)
+    {
+        Debug.Log("OnEeY called");
+        if (context.action.triggered)
+        {
+            eeY = context.ReadValue<float>();
+        }
+        else if (context.canceled)
+        {
+            eeY = 0;
+        }
+    }
+
+    public void OnBaseX(InputAction.CallbackContext context)
+    {
+        Debug.Log("OnBaseX called");
+        if (context.action.triggered)
+        {
+            baseX = context.ReadValue<float>();
+        }
+        else if (context.canceled)
+        {
+            baseX = 0;
+        }
+    }
+
+    public void OnBaseRotate(InputAction.CallbackContext context)
+    {
+        Debug.Log("OnBaseRotate called");
+        if (context.action.triggered)
+        {
+            baseRotate = context.ReadValue<float>();
+        }
+        else if (context.canceled)
+        {
+            baseRotate = 0;
+        }
+    }
+
+    public void OnEeRoll(InputAction.CallbackContext context)
+    {
+        Debug.Log("OnEeRoll called");
+        if (context.action.triggered)
+        {
+            eeRoll = context.ReadValue<float>();
+        }
+        else if (context.canceled)
+        {
+            eeRoll = 0;
+        }
+    }
+
+    public void OnEePitch(InputAction.CallbackContext context)
+    {
+        Debug.Log("OnEePitch called");
+        if (context.action.triggered)
+        {
+            eePitch = context.ReadValue<float>();
+        }
+        else if (context.canceled)
+        {
+            eePitch = 0;
+        }
+    }
+
+    public void OnCameraUpDown(InputAction.CallbackContext context)
+    {
+        Debug.Log("OnEeCameraUpDown called");
+        if (context.action.triggered)
+        {
+            tilt = context.ReadValue<float>();
+        }
+        else if (context.canceled)
+        {
+            tilt = 0;
         }
     }
 }
--- UnityProject/Assets/Scripts/Ros/PubSub/PubUnityControl.cs
+++ UnityProject/Assets/Scripts/Ros/PubSub/PubUnityControl.cs
@@ -30,7 +30,14 @@ public class PubUnityControl : MonoBehaviour
     private bool isConnected = false;
     bool flag;
 
-    
+    static double MAX_BASE_X = 0.7;         // Max translational motion that the base can do is 0.7 m/s
+    static double MAX_BASE_THETA = 3.14;    // Max rotational motion that the base can do is 3.14 rad/s
+    double threshold;                                           // Joystick sensitivity threshold
+    //static bool R_button_pressed = false;
+    //static bool L_button_pressed = false;
+    static bool switch_cmd = false;
+    static bool switch_cmd_last_state = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -56,11 +63,104 @@ public class PubUnityControl : MonoBehaviour
         isConnected = false;
     }
 
-    //ispressedで反応するようにする
     //連続でPubしたいメッセージ
     public void PublishJoy()
     {
-        //arm
+        // Check the speed_cmd
+        if (inputMng.speedInc)
+            controlMsg.speed_cmd = LocobotJoyMsg.SPEED_INC;
+        else if (inputMng.speedDec)
+            controlMsg.speed_cmd = LocobotJoyMsg.SPEED_DEC;
+        else controlMsg.speed_cmd = 0;
+
+        // Check the speed_toggle_cmd
+        if (inputMng.speedCourse)
+            controlMsg.speed_toggle_cmd = LocobotJoyMsg.SPEED_COURSE;
+        else if (inputMng.speedFine)
+            controlMsg.speed_toggle_cmd = LocobotJoyMsg.SPEED_FINE;
+        else controlMsg.speed_toggle_cmd = 0;
+
+        // Check the base_x_cmd
+        controlMsg.base_x_cmd = inputMng.baseX * MAX_BASE_X;
+
+        // Check for the first time the R or L buttons are pressed
+        // This is necessary to fix a potential bug in the 'joy' package - refer to https://github.com/ros-drivers/joystick_drivers/issues/155
+        //if (inputMng.baseRotate>0 && R_button_pressed == false)
+        //    R_button_pressed = true;
+        //if (inputMng.baseRotate<0 && L_button_pressed == false)
+        //    L_button_pressed = true;
+
+        // Check the base_theta_cmd
+        //if (L_button_pressed && R_button_pressed)
+            controlMsg.base_theta_cmd = inputMng.baseRotate / 2.0 * MAX_BASE_THETA;
+        //else if (L_button_pressed && !R_button_pressed)
+        //    controlMsg.base_theta_cmd = (1.0 - inputMng.baseRotate) / 2.0 * MAX_BASE_THETA;
+        //else if (!L_button_pressed && R_button_pressed)
+        //    controlMsg.base_theta_cmd = (inputMng.baseRotate - 1.0) / 2.0 * MAX_BASE_THETA;
+
+        // Check the pan_cmd
+        if (inputMng.pan > 0)
+            controlMsg.pan_cmd = LocobotJoyMsg.PAN_CCW;
+        else if (inputMng.pan < 0)
+            controlMsg.pan_cmd = LocobotJoyMsg.PAN_CW;
+        else controlMsg.pan_cmd = 0;
+
+        // Check the tilt_cmd
+        if (inputMng.tilt > 0)
+            controlMsg.tilt_cmd = LocobotJoyMsg.TILT_DOWN;
+        else if (inputMng.tilt < 0)
+            controlMsg.tilt_cmd = LocobotJoyMsg.TILT_UP;
+        else controlMsg.tilt_cmd = 0;
+
+        // Check if the camera pan-and-tilt mechanism should be reset
+        if (inputMng.panTiltHome)
+        {
+            controlMsg.pan_cmd = LocobotJoyMsg.PAN_TILT_HOME;
+            controlMsg.tilt_cmd = LocobotJoyMsg.PAN_TILT_HOME;
+        }
+
+        // Check the ee_y_cmd
+        if (inputMng.eeY > 0)
+            controlMsg.ee_y_cmd = LocobotJoyMsg.EE_Y_INC;
+        else if (inputMng.eeY < 0)
+            controlMsg.ee_y_cmd = LocobotJoyMsg.EE_Y_DEC;
+        else controlMsg.ee_z_cmd = 0;
+
+        // Check the ee_z_cmd
+        if (inputMng.eeZ > 0)
+            controlMsg.ee_z_cmd = LocobotJoyMsg.EE_Z_INC;
+        else if (inputMng.eeZ < 0)
+            controlMsg.ee_z_cmd = LocobotJoyMsg.EE_Z_DEC;
+        else controlMsg.ee_z_cmd = 0;
+
+        // Check the ee_roll_cmd
+        if (inputMng.eeRoll > 0)
+            controlMsg.ee_roll_cmd = LocobotJoyMsg.EE_ROLL_CW;
+        else if (inputMng.eeRoll < 0)
+            controlMsg.ee_roll_cmd = LocobotJoyMsg.EE_ROLL_CCW;
+        else controlMsg.ee_roll_cmd = 0;
+
+        // Check the ee_pitch_cmd
+        if (inputMng.eePitch > 0)
+            controlMsg.ee_pitch_cmd = LocobotJoyMsg.EE_PITCH_UP;
+        else if (inputMng.eePitch < 0)
+            controlMsg.ee_pitch_cmd = LocobotJoyMsg.EE_PITCH_DOWN;
+        else controlMsg.ee_pitch_cmd=0;
+
+        // Check the waist_cmd
+        if (inputMng.waistRotate>0)
+            controlMsg.waist_cmd = LocobotJoyMsg.WAIST_CCW;
+        else if (inputMng.waistRotate<0)
+            controlMsg.waist_cmd = LocobotJoyMsg.WAIST_CW;
+        else controlMsg.waist_cmd = 0;
+
+        // Check the gripper_cmd
+        if (inputMng.gripperClose)
+            controlMsg.gripper_cmd = LocobotJoyMsg.GRIPPER_CLOSE;
+        else if (inputMng.gripperOpen)
+            controlMsg.gripper_cmd = LocobotJoyMsg.GRIPPER_OPEN;
+        else controlMsg.gripper_cmd = 0;
+
         // Check the pose_cmd
         if (inputMng.armHomePose)
             controlMsg.pose_cmd = LocobotJoyMsg.HOME_POSE;
@@ -68,17 +168,15 @@ public class PubUnityControl : MonoBehaviour
             controlMsg.pose_cmd = LocobotJoyMsg.SLEEP_POSE;
         else controlMsg.pose_cmd = 0;
 
+        // Check the gripper_pwm_cmd
+        if (inputMng.gripperPwm > 0)
+            controlMsg.gripper_pwm_cmd = LocobotJoyMsg.GRIPPER_PWM_INC;
+        else if (inputMng.gripperPwm < 0)
+            controlMsg.gripper_pwm_cmd = LocobotJoyMsg.GRIPPER_PWM_DEC;
+        else controlMsg.gripper_pwm_cmd = 0;
 
-        //turret
-
-        //base
-
-        //gripper
-
-        //if (prevJoyMsg != controlMsg)
-        
-            Debug.LogWarning("PublishedJoyMsg!!");
-            ros.Publish(ControlTopic, controlMsg);
+        //Debug.LogWarning("PublishedJoyMsg!!");
+        ros.Publish(ControlTopic, controlMsg);
         
         m_LastPublishTimeSeconds = RosClock.FrameStartTimeInSeconds;
     }
